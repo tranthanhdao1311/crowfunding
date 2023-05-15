482682478846a8eccc096fb75c21671f42684273
tranthanhdao <tranthanhdao1311@gmail.com>
2023-05-15 15:53:22 +0700

    .

--- src/layout/Header/HeaderDashBoard.js
+++ src/layout/Header/HeaderDashBoard.js
@@ -27,7 +27,7 @@ const HeaderDashBoard = () => {
           >
             <img
               className="w-[40px] h-[40px] sm:w-[52px] sm:h-[52px]"
-              srcSet="/logo.png 2x"
+              src="/logo.png"
               alt=""
             />
           </Link>
