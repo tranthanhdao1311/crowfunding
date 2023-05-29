6379aeb09e8e44c45a3ea8800e2aafb78555e949
tranthanhdao <tranthanhdao1311@gmail.com>
2023-05-30 02:33:56 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
@@ -10,16 +11,21 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
 
-root.render(
+// root.render(
+
+ReactDOM.render(
   <Provider store={store}>
     <App />
     <ToastContainer></ToastContainer>
     <ScrollToTop></ScrollToTop>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
+
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/modules/Payment/ContributionSummury.js
+++ src/modules/Payment/ContributionSummury.js
@@ -96,7 +96,7 @@ const ContributionSummury = ({ data }) => {
         >
           Gửi thanh toán
         </Button> */}
-        <PayPalButton
+        {/* <PayPalButton
           amount={Number(usdAmount.toFixed(2))}
           // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
           onSuccess={(details, data) => {
@@ -116,6 +116,40 @@ const ContributionSummury = ({ data }) => {
           }}
           onError={() => {
             alert("Paypal error");
+          }}
+        /> */}
+        <PayPalButton
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    currency_code: "USD",
+                    value: "0.01",
+                  },
+                },
+              ],
+              // application_context: {
+              //   shipping_preference: "NO_SHIPPING" // default is "GET_FROM_FILE"
+              // }
+            });
+          }}
+          onApprove={(data, actions) => {
+            // Capture the funds from the transaction
+            return actions.order.capture().then(function (details) {
+              // Show a success message to your buyer
+              alert(
+                "Transaction completed by " + details.payer.name.given_name
+              );
+
+              // OPTIONAL: Call your server to save the transaction
+              return fetch("/paypal-transaction-complete", {
+                method: "post",
+                body: JSON.stringify({
+                  orderID: data.orderID,
+                }),
+              });
+            });
           }}
         />
       </div>
